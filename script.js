ef326759016970b5eaabb569cba6229cc125b2f9
Anish Joshi <anish@parakhi.com>
2015-05-01 20:43:48 +0545

    add handler for blank cat id

--- script.js
+++ script.js
@@ -36,26 +36,27 @@ $(document).ready(function(){
 	});
 	});
 	function getDataFromParam() {
+
 		console.log("wow");
 		var catid = getParameterByName('catid');
+		if(catid != ""){
+			$(".spinner").show();
+				    $.get(apiurl+"?task=incidents&by=catid&id="+catid, function(data, status){
+					var incidents = data.payload;
 
-		$(".spinner").show();
-			    $.get(apiurl+"?task=incidents&by=catid&id="+catid, function(data, status){
-				var incidents = data.payload;
+					console.log(data.payload);	
 
-				console.log(data.payload);	
+					var source = $('#data-template').html();
+				    var template = Handlebars.compile(source);
+				    var context = incidents;
 
-				var source = $('#data-template').html();
-			    var template = Handlebars.compile(source);
-			    var context = incidents;
-
-			    $('#container').html(template(context));
-			    $(".spinner").hide();
-			    $("#sharelink").val("http://anishjoshi.github.io/eqhelp/?catid="+catid);
+				    $('#container').html(template(context));
+				    $(".spinner").hide();
+				    $("#sharelink").val("http://anishjoshi.github.io/eqhelp/?catid="+catid);
 
 
-			});
-
+				});
+		}
 	}
 
 function getParameterByName(name) {
